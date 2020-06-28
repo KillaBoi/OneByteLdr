f1661b08b9101cfaa8b28b61178ac3eef0816f51
2Eggs <46409262+2Eggsss@users.noreply.github.com>
2020-06-28 16:57:28 +0100

    Update Bypass.bat

--- Bypass.bat
+++ Bypass.bat
@@ -1,4 +1,21 @@
 echo off
+echo -------------------------------------------------------------------------------------------------------
+echo                                       OneByteLdr - By Daniel Krupinski
+echo -------------------------------------------------------------------------------------------------------
+echo.
+echo.
+echo.
+
 python OneByteLdr.py
-echo If there is an error above, restart this with administrator privileges!
+echo.
+echo.
+echo.
+echo -------------------------------------------------------------------------------------------------------
+echo                      If it says "Process XXXXX is being debugged" then congratulations!
+echo.
+echo                      If there is an error, try running this script as an administrator.
+echo -------------------------------------------------------------------------------------------------------
+echo.
+echo.
+echo.
 PAUSE
